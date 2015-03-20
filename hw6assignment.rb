9ac63a934d8b2b12909bea2c1247fbbeff8a21e4
Luuk Veenis <luukveenis@shaw.ca>
2015-03-19 17:55:47 -0700

    Add new pieces to game
    1) 1x5 straight piece
    2) 3 block 'L' shape
    3) 4x4 block with 1 extra attached to it
    
    Replaced board initialize method in order to select first game piece to
    be of type MyPiece. Similarly, replace next_piece method to select a
    MyPiece.

--- hw6assignment.rb
+++ hw6assignment.rb
@@ -4,19 +4,37 @@
 # part of your solution.
 
 class MyPiece < Piece
-  # The constant All_My_Pieces should be declared here
 
-  # your enhancements here
+  All_My_Pieces = All_Pieces + [[[[0, 0], [-1, 0], [-2, 0], [1, 0], [2, 0]],
+                                [[0, 0], [0, -1], [0, -2], [0, 1], [0, 2]]],
+                                rotations([[0, 0], [0, 1], [1, 0]]),
+                                rotations([[0, 0], [1, 0], [2, 0], [0, 1], [1, 1]])]
 
+  def self.next_piece(board)
+    MyPiece.new(All_My_Pieces.sample, board)
+  end
 end
 
 class MyBoard < Board
+
+  def initialize (game)
+    @grid = Array.new(num_rows) { Array.new(num_columns) }
+    @current_block = MyPiece.next_piece(self)
+    @score = 0
+    @game = game
+    @delay = 500
+  end
 
   def rotate_180
     if !game_over? and @game.is_running?
       @current_block.move(0, 0, 2)
     end
     draw
+  end
+
+  def next_piece
+    @current_block = MyPiece.next_piece(self)
+    @current_pos = nil
   end
 end
 
