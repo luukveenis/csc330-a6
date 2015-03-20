8d3a0ea49d4cb1a5ce676af2858f15a5ab6f0074
Luuk Veenis <luukveenis@shaw.ca>
2015-03-19 21:39:27 -0700

    Delete garbage text

--- hw6assignment.rb
+++ hw6assignment.rb
@@ -1,8 +1,5 @@
 # Programming Languages, Homework 6, hw6runner.rb
 
-# This is the only file you turn in, so do not modify the other files as
-# part of your solution.
-
 class MyPiece < Piece
 
   All_My_Pieces = All_Pieces + [[[[0, 0], [-1, 0], [-2, 0], [1, 0], [2, 0]],
